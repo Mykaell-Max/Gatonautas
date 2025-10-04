6fa62645b8143254983970d047fb4bdb5ae1a41c
BiancaMalta <biancass_malta@hotmail.com>
2025-10-04 18:08:39 -0300

    Alterações na Home e Pop-up

--- web/front-end/src/App.tsx
+++ web/front-end/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import Home from "./Home";
-import AuthCard from "./components/AuthCard";
+import Home from "./pages/Home/Home.tsx";
+import AuthCard from "./components/AuthCard.tsx";
 
 function App() {
   const [showAuth, setShowAuth] = useState(false);
