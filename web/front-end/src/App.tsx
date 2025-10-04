319617119b039b5d10518e34b07a221be28c5155
BrenoPalma <brenomiele@ufu.br>
2025-10-04 20:23:57 -0300

    Correção botão misterioso

--- web/front-end/src/App.tsx
+++ web/front-end/src/App.tsx
@@ -1,6 +1,5 @@
 import { AppRoutes } from "./routes";
 import Navbar from "./components/Navbar/Navbar";
-import AuthCard from "./components/AuthCard";
 
 function App() {
   return (
--- web/front-end/src/components/Navbar/Navbar.tsx
+++ web/front-end/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import AuthCard from "../AuthCard"; // ajuste o caminho conforme sua estrutura
-import "./Navbar.css"
+import AuthCard from "../AuthCard"; 
+import "./Navbar.css";
 
 const Navbar: React.FC = () => {
   const [showAuthCard, setShowAuthCard] = useState(false);
 
   const handleLoginClick = () => setShowAuthCard(true);
-  const handleCloseAuthCard = () => setShowAuthCard(false);
 
   return (
     <>
@@ -23,46 +22,7 @@ const Navbar: React.FC = () => {
         </ul>
       </nav>
 
-      {showAuthCard && (
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: "rgba(0,0,0,0.7)",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            zIndex: 9999,
-          }}
-          onClick={handleCloseAuthCard} // fecha clicando no overlay
-        >
-          <div onClick={(e) => e.stopPropagation()} style={{ position: "relative" }}>
-            {/* Botão de fechar */}
-            <button
-              onClick={handleCloseAuthCard}
-              style={{
-                position: "absolute",
-                top: "-40px",
-                right: "0",
-                background: "none",
-                border: "none",
-                color: "white",
-                fontSize: "32px",
-                cursor: "pointer",
-                fontWeight: "bold",
-              }}
-            >
-              ✕
-            </button>
-
-            {/* Componente de login/signup */}
-            <AuthCard />
-          </div>
-        </div>
-      )}
+      {showAuthCard && <AuthCard />}
     </>
   );
 };
