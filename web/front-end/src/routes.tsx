80dad1c85186bd90c9c0d550be6fbcd010cdc8d4
BiancaMalta <biancass_malta@hotmail.com>
2025-10-05 09:34:03 -0300

    nova alt botao link

--- web/front-end/src/routes.tsx
+++ web/front-end/src/routes.tsx
@@ -5,6 +5,7 @@ import Training from "./pages/Training/Training"
 import Explore from "./pages/Explore/Explore";
 import About from "./pages/About/About";
 import UploadData from "./pages/Explore/UploadData/UploadData";
+import Quiz from "./pages/Quiz/Quiz";
 
 export const AppRoutes = () =>{
     return(
@@ -15,6 +16,7 @@ export const AppRoutes = () =>{
                 <Route path="/Explore" element={<Explore/>} />
                 <Route path="/About" element={<About/>} />
                 <Route path="/UploadData" element={<UploadData/>} />
+                <Route path="/Quiz" element={<QuizView />} />
 
             </Routes>
     )
