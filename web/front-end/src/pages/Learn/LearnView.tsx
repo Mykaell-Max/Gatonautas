ef2c070b058ee72bab44233463d2a09702f718e8
BiancaMalta <biancass_malta@hotmail.com>
2025-10-04 21:01:49 -0300

    pag learn correção espaçamento

--- web/front-end/src/pages/Learn/LearnView.tsx
+++ web/front-end/src/pages/Learn/LearnView.tsx
@@ -10,10 +10,14 @@ const LearnView: React.FC = () => {
           <div className="learn-text">
             <h1>Learn.</h1>
             <p>
-              From the fundamentals of planetary transits to Machine Learning tools, dive into interactive lessons and challenges. Empower your curiosity and become part of the next generation of explorers.
+              From the fundamentals of planetary transits to Machine Learning tools, dive into interactive lessons and challenges.
             </p>
+            <p>Empower your curiosity and become part of the next generation of explorers.</p>
             <div className="learn-buttons">
               <button className="training">Training</button>
+            </div>
+            <div className="learn-buttons">
+            <p>Ready to go? Classify real data and help new discoveries!</p>  
               <button className="classify">Classify</button>
             </div>
           </div>
